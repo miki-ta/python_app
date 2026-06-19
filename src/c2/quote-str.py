274164673354347d1662dfbfa894ba08ae4f0cfd
miki-ta <hirake.ponk.kids@gmail.com>
2026-06-19 13:26:24 +0900

    Delete src/c2 directory

--- src/c2/quote-str.py
+++
@@ -1,6 +0,0 @@
-# シングルクォートを文字列で使いたい場合
-print("I can't speak English.")
-
-# シングルクォートを文字列で使いたい場合
-print('He said, "I play the piano".')
-
--- src/c2/while-else2.py
+++
@@ -1,8 +0,0 @@
-i = 3
-while i < 5:
-    print("while(in)=", i)
-    if i == 2: break
-    i = i + 1
-else:
-    print("while(else)=", i)
-
