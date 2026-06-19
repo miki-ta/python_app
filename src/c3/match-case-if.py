ca0eb9552429bc91c4a1a9c0b9ee3bb061dc5b42
miki-ta <hirake.ponk.kids@gmail.com>
2026-06-19 13:26:34 +0900

    Delete src/c3 directory

--- src/c3/match-case-if.py
+++
@@ -1,8 +0,0 @@
-for no in range(10, 21):
-    match no:
-        case i if no % 3 == 0 and no % 5 == 0:
-            print(f"{no}は…3の倍数かつ5の倍数")
-        case i if no % 3 == 0:
-            print(f"{no}は…3の倍数")
-        case i if no % 5 == 0:
-            print(f"{no}は…5の倍数")
