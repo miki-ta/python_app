755836e0dde891a69770a5aeee5d197829b70fef
miki-ta <hirake.ponk.kids@gmail.com>
2026-06-30 14:43:35 +0900

    Delete src/c3 directory

--- src/c3/args-dict.py
+++
@@ -1,6 +0,0 @@
-def print_args(**args):
-    print(args)
-
-print_args(a=30, b=50, c=40)
-print_args(aa="hoge", bb="fuga")
-
--- src/c3/fruits-list.py
+++
@@ -1,4 +0,0 @@
-fruits = ["Apple", "Banana", "Mango", "Orange"]
-for i in fruits:
-    print("「" + i + "」が好き!")
-
